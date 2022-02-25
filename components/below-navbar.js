0bb799f041d051eee53317a2f1611e156d85ae00
iam-riyaz <riyazahmad.online@gmail.com>
2022-02-25 22:35:05 +0530

    Create below-navbar.js

---
+++ components/below-navbar.js
@@ -0,0 +1,20 @@
+ function mini_navbar(){
+     return ` <div id="below-navbar">
+
+     <div id="left-side">
+
+         <a href="discover-collection.html">Collections</a>
+         <a href="all-smallcase.html">All smallcases</a>
+         <a href="managers.html">Managers</a>
+
+     </div>
+
+     <div id="right-side">
+         <input type="text" id="search-bar" placeholder="Try 'All Weather' or 'Infosys'">
+     </div>
+       
+   </div>
+   <hr id="myhr">`
+ }
+
+ export {mini_navbar}
