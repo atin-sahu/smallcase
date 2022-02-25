5689727e36391c15d70b8bc635ab4fb7ad2f0ac9
amankatiyar321 <97542315+amankatiyar321@users.noreply.github.com>
2022-02-26 02:02:50 +0530

    Aman_update
    New_files

---
+++ components/words.js
@@ -0,0 +1,16 @@
+
+
+
+
+function side(){
+
+    return `
+    <h1>Investing ecosystem for<br>ambitious brands</h1>
+   <p>Connect into India’s fastest growing <br>investment product layer and offer <br>modern products, seamless transactions<br>and delightful experiences </p>
+    
+    `
+}
+
+
+
+export default side();
